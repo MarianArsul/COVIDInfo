9e35aa4c489042eb859f85d639cdd34c512270af
Milovan <marian.arsul00@e-uvt.ro>
2021-04-04 01:51:35 +0300

    Multiple additions and changes

--- CoronaVirus/Home/Cards/DocumenteUtileView.swift
+++ CoronaVirus/Home/Cards/DocumenteUtileView.swift
@@ -6,78 +6,140 @@
 //
 
 import SwiftUI
+import Swift
+import SwiftUITrackableScrollView
+
 
 struct DocumenteUtileView: View {
     
+    @State var showDocumentActions = false
+    @State private var scrollViewContentOffset = CGFloat(0) //80
+    @State var elementOpacity : Double = 0
+    
     var body: some View {
-        ScrollView(showsIndicators: false){
-            
-            Text("Documente utile")
-                .foregroundColor(Color.white)
-                .font(.system(size: 30, weight: .bold))
-                .offset(y: 70)
-                .offset(x: -50)
-                .frame(width: 400)
-            
-            Link(destination: URL(string: "https://cetrebuiesafac.ro/modele-declaratii")!){
-                HStack{
-                    Image(systemName: "info.circle")
-                        .font(.system(size: 13, weight: .bold))
-                        .offset(x: 90)
-                        .foregroundColor(Color.black)
+        
+        ZStack {
+                VStack{
+                    Text("Documente utile")
+                        .foregroundColor(Color.white)
+                        .font(.system(size: 30, weight: .bold))
+                        .offset(y: 70)
+                        .offset(x: -50)
+                        .frame(width: 400)
+                        .opacity(0.01 * Double(scrollViewContentOffset))
+                        
                     
-                    Text("Pentru mai multe informații accesează www.cetrebuiesafac.ro")
-                        .font(.system(size: 10, weight: .bold))
-                        .offset(x: 90)
-                        .foregroundColor(Color.black)
-                }
-                .frame(width: 550, height: 30)
-                .background(
-                    RoundedRectangle(cornerRadius: 20)
-                        .fill(Color(#colorLiteral(red: 0.6870401502, green: 0.8114628196, blue: 0.9983285069, alpha: 1))))
-                .offset(x: -87)
-                .offset(y: 71)
-            }
-            VStack{
-                
-                Text("Declarația pe propria răspundere")
-                    .frame(width: 350)
-                    .font(.system(size: 18, weight: .bold))
-                    .foregroundColor(Color.white)
-                    .offset(y: 40)
-                    .offset(x: -30)
-                
-                VStack(spacing: 20){
-                    ForEach(documentDPRData){ item in
-                        DocumentCardView(documentData: item)
-                            .offset(y: -150)
+                    Link(destination: URL(string: "https://cetrebuiesafac.ro/modele-declaratii")!){
+                        HStack{
+                            Image(systemName: "info.circle")
+                                .font(.system(size: 13, weight: .bold))
+                                .offset(x: 90)
+                                .foregroundColor(Color.black)
+                                .opacity(0.01 * Double(scrollViewContentOffset))
+                            
+                            Text("Pentru mai multe informații accesează www.cetrebuiesafac.ro")
+                                .font(.system(size: 10, weight: .bold))
+                                .offset(x: 90)
+                                .foregroundColor(Color.black)
+                                .opacity(0.01 * Double(scrollViewContentOffset))
+                        }
+                        .frame(width: 550, height: 30)
+                        .background(
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(Color(#colorLiteral(red: 0.6870401502, green: 0.8114628196, blue: 0.9983285069, alpha: 1))))
+                        .offset(x: -87)
+                        .offset(y: 71)
                     }
+                    .opacity(0.01 * Double(scrollViewContentOffset))
+                }
+                .background(Rectangle() .fill(Color(#colorLiteral(red: 0, green: 0.4227619767, blue: 0.890914619, alpha: 1)).opacity(0.01 * Double(scrollViewContentOffset))) .offset(y: 50) .frame(height: 120))
+                .offset(y: -420)
+                .zIndex(2)
+                .animation(Animation.easeInOut(duration: 4))
+            
+            TrackableScrollView(.vertical, showIndicators: false, contentOffset: $scrollViewContentOffset){
+                
+                    VStack{
+                        Text("Documente utile")
+                            .foregroundColor(Color.white)
+                            .font(.system(size: 30, weight: .bold))
+                            .offset(y: 70)
+                            .offset(x: -50)
+                            .frame(width: 400)
+                            .opacity(1 - 0.02 * Double(scrollViewContentOffset))
+                            
+                        
+                        Link(destination: URL(string: "https://cetrebuiesafac.ro/modele-declaratii")!){
+                            HStack{
+                                Image(systemName: "info.circle")
+                                    .font(.system(size: 13, weight: .bold))
+                                    .offset(x: 90)
+                                    .foregroundColor(Color.black)
+                                    .opacity(1 - 0.02 * Double(scrollViewContentOffset))
+                                
+                                Text("Pentru mai multe informații accesează www.cetrebuiesafac.ro")
+                                    .font(.system(size: 10, weight: .bold))
+                                    .offset(x: 90)
+                                    .foregroundColor(Color.black)
+                                    .opacity(1 - 0.02 * Double(scrollViewContentOffset))
+                            }
+                            .frame(width: 550, height: 30)
+                            .background(
+                                RoundedRectangle(cornerRadius: 20)
+                                    .fill(Color(#colorLiteral(red: 0.6870401502, green: 0.8114628196, blue: 0.9983285069, alpha: 1)).opacity(1 - 0.02 * Double(scrollViewContentOffset))))
+                            .offset(x: -87)
+                            .offset(y: 71)
+                        }
+                    }
+                    .offset(y: 40)
+                
+                VStack{
                     
-                    Text("Adeverință de angajator")
+                    Text("Declarația pe propria răspundere")
                         .frame(width: 350)
                         .font(.system(size: 18, weight: .bold))
                         .foregroundColor(Color.white)
-                        .offset(y: 70)
-                        .offset(x: -70)
+                        .offset(y: 40)
+                        .offset(x: -30)
                     
-                    ForEach(documentAAData){ item in
-                        DocumentCardView(documentData: item)
-                            .offset(y: -120)
+                        ForEach(documentDPRData){ item in
+                            Button(action: {self.showDocumentActions.toggle()}, label: {
+                                DocumentCardView(documentData: item, showDocumentActions: showDocumentActions ? true : false)
+                                    .offset(y: -150)
+                            })
+                        }
+                        
+                        
+                        Text("Adeverință de angajator")
+                            .frame(width: 350)
+                            .font(.system(size: 18, weight: .bold))
+                            .foregroundColor(Color.white)
+                            .offset(y: 50)
+                            .offset(x: -70)
+                        
+                        ForEach(documentAAData){ item in
+                                DocumentCardView(documentData: item, showDocumentActions: true)
+                                    .offset(y: -150)
+                        }
+                    
+                    
+                    ForEach(1..<45){ i in
+                        Spacer()
                     }
                 }
-                
-                ForEach(1..<45){ i in
-                    Spacer()
-                }
+                .frame(width: 400)
+                .background(
+                    RoundedRectangle(cornerRadius: 38)
+                        .fill(Color(#colorLiteral(red: 0.3783457875, green: 0.6356819868, blue: 0.9972093701, alpha: 1)).opacity(0.8)))
+                .offset(y: 300)
             }
-            .frame(width: 400)
-            .background(
-                RoundedRectangle(cornerRadius: 38)
-                    .fill(Color(#colorLiteral(red: 0.3783457875, green: 0.6356819868, blue: 0.9972093701, alpha: 1)).opacity(0.8)))
-            .offset(y: 250)
+                .background(Image("documenteUtile").aspectRatio(contentMode: .fill).offset(y: 10).offset(x: 280))
+                .frame(width: screen.width)
+                .edgesIgnoringSafeArea(.all)
+            .offset(x: -80)
+                .zIndex(1)
         }
-            .background(Image("documenteUtile").aspectRatio(contentMode: .fill).offset(y: 10).offset(x: 280))
-        .frame(width: screen.width)
+        .edgesIgnoringSafeArea(.all)
     }
     
 }
@@ -92,36 +154,46 @@ struct DocumentCardView: View {
     
     var documentData : Document
     
+    var showDocumentActions : Bool
+    
     var body: some View {
         VStack(){
-            documentData.imagine
-                .resizable()
-                .frame(width: 350, height: 210)
-                .cornerRadius(20)
-                .offset(y: -15)
-                .zIndex(1)
             
-            /*HStack(spacing: 20){
-                ForEach(0..<3){ i in
-                    VStack {
-                        Image(systemName: documentActionsData[i].icon)
-                            .font(.system(size: 40, weight: .bold))
-                            .frame(width: 80, height: 70)
-                            .background(Circle().fill(Color.white))
-                            .foregroundColor(Color(#colorLiteral(red: 0.6870401502, green: 0.8114628196, blue: 0.9983285069, alpha: 1)))
-                        
-                        Text(documentActionsData[i].action)
-                            .foregroundColor(Color.white)
-                            .font(.system(size: 15, weight: .bold))
-                            .offset(y: 10)
-                    }
-                    .zIndex(3)
-                }
+            HStack {
+                documentData.imagine
+                    .resizable()
+                    .frame(width: 250, height: 170)
+                    .offset(y: -10)
             }
-            .frame(width: 350, height: 210)
-            .background(Color.white.opacity(0.1).blur(radius: 30))
-            .offset(y: -230)
-            .zIndex(2)*/
+            .frame(width: 350, height: 190)
+            .background(
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color(#colorLiteral(red: 0.8402631879, green: 0.902616322, blue: 0.9990279078, alpha: 1)))
+                    .offset(y: -20)
+            )
+            
+            if showDocumentActions {
+                HStack(spacing: 20){
+                    ForEach(0..<3){ i in
+                        VStack {
+                            Image(systemName: documentActionsData[i].icon)
+                                .font(.system(size: 40, weight: .bold))
+                                .frame(width: 80, height: 70)
+                                .background(Circle().fill(Color.white))
+                                .foregroundColor(Color(#colorLiteral(red: 0.6870401502, green: 0.8114628196, blue: 0.9983285069, alpha: 1)))
+                            
+                            Text(documentActionsData[i].action)
+                                .foregroundColor(Color.white)
+                                .font(.system(size: 15, weight: .bold))
+                                .offset(y: 10)
+                        }
+                        .zIndex(3)
+                    }
+                }
+                .frame(width: 350, height: 210)
+                .background(Color.white.opacity(0.1).blur(radius: 30))
+                .offset(y: -230)
+            }
             
             
             Text(documentData.titlu)
@@ -131,12 +203,12 @@ struct DocumentCardView: View {
                 
             
         }
-        .frame(width: 350, height: 250)
+        .frame(width: 350, height: 230)
         .background(
             RoundedRectangle(cornerRadius: 20)
                 .fill(Color(#colorLiteral(red: 0.6870401502, green: 0.8114628196, blue: 0.9983285069, alpha: 1))))
-        .offset(y: 210)
-        .zIndex(1)
+        .offset(y: showDocumentActions ? 310 : 220)
+        .padding(.bottom, showDocumentActions ? -30 : 30)
     }
 }
 
@@ -147,13 +219,14 @@ struct Document : Identifiable {
 }
 
 let documentDPRData = [
-    Document(titlu: "Indiferent de rata de incidență", imagine: Image("imagePlaceholder")),
-    Document(titlu: "Rată de incidență > 4% sau 7,5%", imagine: Image("imagePlaceholder"))
+    Document(titlu: "Indiferent de rata de incidență", imagine: Image("document1")),
+    Document(titlu: "Rată de incidență > 4% sau 7,5%", imagine: Image("document2")),
+    Document(titlu: "Tranzitarea unei localități carantinate", imagine: Image("document5"))
 ]
 
 let documentAAData = [
-    Document(titlu: "Indiferent de rata de incidență", imagine: Image("imagePlaceholder")),
-    Document(titlu: "Rată de incidență > 4% sau 7,5%", imagine: Image("imagePlaceholder"))
+    Document(titlu: "Indiferent de rata de incidență", imagine: Image("document3")),
+    Document(titlu: "Rată de incidență > 4% sau 7,5%", imagine: Image("document4"))
 ]
 
 struct DocumentActions : Identifiable{
