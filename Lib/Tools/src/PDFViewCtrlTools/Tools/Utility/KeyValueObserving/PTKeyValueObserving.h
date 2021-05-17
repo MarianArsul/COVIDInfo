6f2ff218e48f13078440e20cc9ddd28db017e544
Milovan <marian.arsul00@e-uvt.ro>
2021-05-17 10:49:13 +0300

    Release

--- Lib/Tools/src/PDFViewCtrlTools/Tools/Utility/KeyValueObserving/PTKeyValueObserving.h
+++
@@ -1,9 +0,0 @@
-//---------------------------------------------------------------------------------------
-// Copyright (c) 2001-2020 by PDFTron Systems Inc. All Rights Reserved.
-// Consult legal.txt regarding legal and license information.
-//---------------------------------------------------------------------------------------
-
-#import "PTKeyValueObservation.h"
-#import "PTKeyValueObservedChange.h"
-
-#import "NSObject+PTKeyValueObserving.h"
